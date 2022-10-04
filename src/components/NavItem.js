19c509830b41393b613bcd9ea7d10928f02ffb9d
Aries <ianorillo21@gmail.com>
2022-10-04 17:46:45 +0800

    assignment day 2

---
+++ src/components/NavItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Flex, Text, Link, Menu, MenuButton } from '@chakra-ui/react';
+
+export default function NavItem({ title, active }) {
+  return (
+    <Flex mt={30} flexDir="column" w="100%">
+      <Menu>
+        <Link
+          w="100%"
+          p={2}
+          borderRadius={5}
+          backgroundColor={active && '#00ADB5'}
+          _hover={{ textDecor: 'none', backgroundColor: '#00ADB5' }}
+        >
+          <MenuButton w="100%">
+            <Flex>
+              <Text fontSize="lg" ml={3}>
+                {title}
+              </Text>
+            </Flex>
+          </MenuButton>
+        </Link>
+      </Menu>
+    </Flex>
+  );
+}
---
+++ src/components/Sidebar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Flex, Text } from '@chakra-ui/react';
+import NavItem from '../components/NavItem';
+
+export default function Sidebar() {
+  return (
+    <Flex
+      h="100vh"
+      w="300px"
+      backgroundColor="#EEEEEE"
+      color="#393E46"
+      boxShadow="0 4px 3px 0 rgba(0, 0, 0, 0.1)"
+      borderRadius="10px"
+      flexDir="column"
+      justifyContent="space-between"
+    >
+      <Flex p="5%" flexDir="column" w="100%">
+        <NavItem title="About Me" />
+        <NavItem title="Projects" active />
+        <NavItem title="Tech Stack" />
+        <NavItem title="Contact" />
+      </Flex>
+
+      <Flex
+        p="5%"
+        w="100%"
+        flexDir="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Text fontSize="lg" fontWeight="bold">
+          ARIES IAN S. ORILLO
+        </Text>
+        <Text fontSize="md">Copyright 2022 &copy;</Text>
+      </Flex>
+    </Flex>
+  );
+}
